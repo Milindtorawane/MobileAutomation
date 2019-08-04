8307ebbe42e83d43ec4712347f9eb6bdd27de4f2
milindt <milind.torawane@infostretch.com>
2019-08-04 13:55:16 +0530

    Added WebAutomation project

---
+++ src/test/java/com/mst/appium/test/LaunchApp.java
@@ -0,0 +1,31 @@
+package com.mst.appium.test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.android.AndroidDriver;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class LaunchApp {
+	
+	
+	static AndroidDriver driver =null ;
+	
+	public static void main (String[]args) throws MalformedURLException{
+		
+		DesiredCapabilities cap= new DesiredCapabilities();
+		cap.setCapability(CapabilityType.BROWSER_NAME, "");
+		cap.setCapability("deviceName","Lenovo k5 note");
+		cap.setCapability("platformName","Android");
+		cap.setCapability("platformVersion","6.0.1");
+		cap.setCapability("appPackage", "");
+		cap.setCapability("appActivity", "");	
+		driver= new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), cap);
+		
+		
+		
+		
+	}
+
+}
